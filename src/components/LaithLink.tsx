96f02828b4dcf39c8a355da7f498eb86c9e8d5df
laithm <laith.masri@gmail.com>
2025-06-08 20:48:14 +0100

    LaithLink component, to make it relatively easier to link (how lazy)

---
+++ src/components/LaithLink.tsx
@@ -0,0 +1,14 @@
+// components/LaithLink.tsx
+export default function LaithLink() {
+  return (
+    <a
+      href="https://www.laithmasri.dev"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="underline hover:text-yellow-400"
+    >
+      Laith Masri
+    </a>
+  );
+}
+// just linking personal website directly without altering code <LaithLink />
